ebf2758f1389a95fb1d5a6a832a033d94a96b843
SabanaSultana <sabanasultana132@gamil.com>
2026-05-11 11:50:38 +0530

    First Commit, added:index.js, models[userModel.js], controllers[user[getUser.js]], configured:config[ db.js],

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express=require('express')
+const cors=require('cors')
+const cookieParser=require('cookie-parser')
+const connectDB = require('./config/db')
+const router=require('./routes/index')
+
+const app=express()
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}
+
+app.use(cors(corsOptions))
+app.use(express.json({ limit: '10mb' }))
+app.use(cookieParser())
+
+app.use('/api', router)
+
+PORT=process.env.PORT || 3000
+
+connectDB().then(()=>{
+    console.log("DB Connection Successful")
+    app.listen(PORT, ()=>{
+        console.log("The Server is running on port", PORT)
+        console.log(`The Server has started at port ${PORT}`)
+    })
+})
---
+++ models/userModel.js
@@ -0,0 +1,51 @@
+const mongoose=require('mongoose')
+
+const userSchema=new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        employeeId:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        password:{
+            type:String,
+            required:true
+        } ,
+        role:{
+            type:String,
+            enum:['Employee','Manager(COE)','Head of Engineering','Admin'],
+            required:true
+        },
+        managerEmployeeId:{
+            type:String,
+            default:null
+        },
+        department:{
+            type:String,
+            enum:[
+                'Mechanical and System Integration',
+                'Virtual Manufacturing',
+                'Smart Manufacturing',
+                'Electrical and Automation',
+                'Hydraulic',
+                'Engineering & System',
+                'Head of Engineering'
+            ],
+            required:true
+        }
+    },
+     {
+        timestamps:true
+    }
+)
+
+module.exports=mongoose.model('User',userSchema)
